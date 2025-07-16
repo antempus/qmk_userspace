7aae18a8811c7edf2668c08ab1cd45f9bcb2ddbf
Robert Steele <rsteele@knock.com>
2025-07-15 22:05:42 -0700

    ok: clean up layers

--- keyboards/planck/rev6/keymaps/antempus/config.h
+++ keyboards/planck/rev6/keymaps/antempus/config.h
@@ -46,4 +46,6 @@
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 // Most tactile encoders have detents every 4 stages
+#define ENCODER_A_PINS { B12 }
+#define ENCODER_B_PINS { B13 }
 #define ENCODER_RESOLUTION 4
--- keyboards/planck/rev6/keymaps/antempus/rules.mk
+++ keyboards/planck/rev6/keymaps/antempus/rules.mk
@@ -1,3 +1,5 @@
 ifeq ($(strip $(AUDIO_ENABLE)), yes)
     SRC += muse.c
 endif
+
+ENCODER_ENABLE = yes
